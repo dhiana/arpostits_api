d88d2ea51b90b56a95f3016bf70c0837dc7b9f35
Dhiana Deva <dhiana.deva@gmail.com>
2014-05-23 03:44:12 -0300

    Implements nested items in Project resource

--- projects/api.py
+++ projects/api.py
@@ -1,10 +1,12 @@
-# myapp/api.py
+from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from .models import Project
 
 
 class ProjectResource(ModelResource):
+    items = fields.ToManyField('items.api.ItemResource', 'item_set',  related_name='project', full=True)
+
     class Meta:
         queryset = Project.objects.all()
         # Very insecure
