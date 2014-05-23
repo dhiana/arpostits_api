f42a455b9dd580a4d20a61803429bcae8ad8644f
Dhiana Deva <dhiana.deva@gmail.com>
2014-05-23 01:44:31 -0300

    Adds progress to Item

--- items/models.py
+++ items/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class Item(models.Model):
     title = models.CharField(max_length=500)
     project = models.ForeignKey('projects.Project')
+    progress = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(100)])
     
     def __unicode__(self):
         return '%i - %s' % (self.id, self.title)
