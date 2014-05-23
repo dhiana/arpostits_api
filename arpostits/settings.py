292109f87111317553883329bddae7d9e316dfa2
Dhiana Deva <dhiana.deva@gmail.com>
2014-05-23 00:07:36 -0300

    Deletes STATICFILES_DIRS

--- arpostits/settings.py
+++ arpostits/settings.py
@@ -97,9 +97,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-            os.path.join(BASE_DIR, 'static'),
-            )
 
 try:
     from local_settings import *
