801e64d07ecf20d93e69b24e92493b604bd376a3
Dhiana Deva <dhiana.deva@gmail.com>
2014-05-26 06:11:27 -0300

    Makes it easier to update items

--- items/admin.py
+++ items/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Item
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+    list_display = ['title', 'marker', 'progress', 'ready', 'blocked']
+    list_editable = ['marker', 'progress', 'ready', 'blocked']
+
+admin.site.register(Item, ItemAdmin)
